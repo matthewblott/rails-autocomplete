9df893ff516e7e5dcafd0ed318e317cc138560f6
Matthew Blott <m4ttb@pm.me>
2024-03-21 11:14:31 +0000

    Changed the view's model to a generic hash.

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,18 +1,21 @@
 class CountriesController < ApplicationController
   def search
-    if params[:q].present?
-      @countries = Country.name_like(params[:q])
+    countries = if params[:q].present?
+      Country.name_like(params[:q])
     else
-      @countries = []
+      []
     end
+
+    items = {}
+    countries.map { |country| items[country.id] = country.name }
 
     respond_to do |format|
       format.turbo_stream do
         render(
           turbo_stream: turbo_stream.update(
-            "search_results",
-            partial: "countries/search_results",
-            locals: {countries: @countries}
+            "country_search_results",
+            partial: "search_results",
+            locals: {items:}
           )
         )
       end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,18 +1,21 @@
 class MoviesController < ApplicationController
   def search
-    if params[:q].present?
-      @movies = Movie.name_like(params[:q])
+    movies = if params[:q].present?
+      Movie.name_like(params[:q])
     else
-      @movies = []
+      []
     end
+
+    items = {}
+    movies.map { |movie| items[movie.id] = movie.name }
 
     respond_to do |format|
       format.turbo_stream do
         render(
           turbo_stream: turbo_stream.update(
             "movie_search_results",
-            partial: "movies/search_results",
-            locals: {movies: @movies}
+            partial: "search_results",
+            locals: {items:}
           )
         )
       end
